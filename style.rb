cd49334b145ef90440fceb1c6b2b0f84b30a748e
AllisonR5 <allicraft05@gmail.com>
2018-07-24 18:28:09 +0000

    automatically backed up by learn

--- style.rb
+++ style.rb
@@ -1,15 +1,13 @@
 def cupid_shuffle
 count = 0 
-loop do 
-if count == 3
-  break
-end
+ 
+ while count < 10 
   puts "to the right, to the right, to the right, to the right"
   puts "to the right, to the right, to ther right"
   puts "now kick! now kick! now nick!"
   puts "now walk it by yourself, now walk it by yourself"
   
-  count = count + 1 
+  count += 1  
   
   puts "#{count} move(s) completed!"
   end 
