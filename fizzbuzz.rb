c6502b3851d300a864101e47dbfa519def74115a
AllisonR5 <allicraft05@gmail.com>
2018-07-24 18:16:30 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+def fizzbuzz(nugget)
+  if nugget %3 == 0 && nugget %5 == 0 
+    puts "fizzbuzz"
+  elsif nugget %3 == 0 
+    puts "fizz"
+  elsif nugget %5 == 0 
+    puts "buzz"
+  else 
+ end 
+end 
+fizzbuzz(15)
---
+++ style.rb
@@ -0,0 +1,6 @@
+loop do 
+  puts "to the right, to the right, to the right, to the right"
+  puts "to the right, to the right, to ther right"
+  puts "now kick! now kick! now nick!"
+  puts "now walk it by yourself, now walk it by yourself"
+end
