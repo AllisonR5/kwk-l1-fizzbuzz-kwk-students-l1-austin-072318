38bfbcba122ac2b134dcf5f2d7ceaeab2f022a71
AllisonR5 <allicraft05@gmail.com>
2018-07-24 18:33:06 +0000

    automatically backed up by learn

--- style.rb
+++ style.rb
@@ -1,13 +1,13 @@
 def cupid_shuffle
 count = 0 
  
- until count > 10 
+ until count = 10 
   puts "to the right, to the right, to the right, to the right"
   puts "to the right, to the right, to ther right"
   puts "now kick! now kick! now nick!"
   puts "now walk it by yourself, now walk it by yourself"
   
-  count += 1  
+  count = count + 1  
   
   puts "#{count} move(s) completed!"
   end 
