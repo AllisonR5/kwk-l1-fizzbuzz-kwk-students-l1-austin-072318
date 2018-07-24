ebbf9ec7a72b75c31f2e468a6d01c61a35212f18
AllisonR5 <allicraft05@gmail.com>
2018-07-24 18:19:50 +0000

    automatically backed up by learn

--- style.rb
+++ style.rb
@@ -1,5 +1,7 @@
-
+def cupid_shuffle
+count = 0 
 loop do 
+  if count == 3
   puts "to the right, to the right, to the right, to the right"
   puts "to the right, to the right, to ther right"
   puts "now kick! now kick! now nick!"
